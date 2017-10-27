7d25cdc39cddc56cdb891f58f81ca41813354f9f
brwitten <bethrwitten@gmail.com>
2017-10-27 12:24:13 -0700

    completed initial set up - confirmed files linked, rendered Hello World

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-// Developer TODO: Define App component defintion here
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld.js';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <header>
+          <h1>Welcome, to Giffaw in React</h1>
+        </header>
+        <HelloWorld />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <p>Hello, globe!</p>
+    )
+  }
+}
+
+export default HelloWorld;
