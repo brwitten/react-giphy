03b822f85e158244ee22fd0a373a90a77d54e676
brwitten <bethrwitten@gmail.com>
2017-10-27 14:46:39 -0700

    wired search bar to store value, finished step 4

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      gifQuery: ""
+    }
+  }
+  storeQuery (e) {
+    console.log("query storing!");
+    this.setState({gifQuery: e.target.value});
+  }
+  submitQuery (e) {
+    this.setState({gifQuery: e.target.value});
+    console.log("state of gif query:" + this.state.gifQuery);
+  }
   render () {
     return (
       <div>
-        <input type="text" placeholder="Search here!"></input>
-        <button>Submit</button>
+        <input type="text" placeholder="Search here!" value={this.state.gifQuery} onChange={(e) => this.storeQuery(e)}/>
+        <button onClick={(e) => this.submitQuery(e)}>Submit</button>
       </div>
     )
   }
